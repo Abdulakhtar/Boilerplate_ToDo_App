4aced4fb930aedada68f35b8a822b12b0dc81603
Two Fast Computer <Two Fast Computer@ABDULLAHAKHTAR>
2025-02-03 12:02:16 +0300

    Add project files.

---
+++ TodoApp/Entities/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Volo.Abp.Domain.Entities;
+
+namespace TodoApp.Entities;
+
+public class TodoItem : BasicAggregateRoot<Guid>
+{
+    public string Text { get; set; }
+}
+
